2239623465e1f807bbf9053d26cce31f47e4499b
Sourav halder <souravh093@gmail.com>
2023-05-01 21:54:30 +0600

    header section markup done

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaMapMarkerAlt, FaEnvelope, FaSearch } from "react-icons/fa";
 import logo from "../../../assets/logo.png";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="max-w-7xl mx-auto">
-      <div>
-        <div>
-          <FaMapMarkerAlt />
+      <div className="flex justify-between items-center py-10">
+        <div className="flex items-center gap-2">
+          <FaMapMarkerAlt className="text-4xl text-blue-400" />
           <div>
             <p>523 Sylvan Ave</p>
             <p>Mountain View, CA 94041 USA</p>
@@ -17,14 +17,24 @@ const Header = () => {
         <div>
           <img src={logo} alt="" />
         </div>
-        <div>
-          <Link to="/login">Login</Link>
-        </div>
+          <Link to="/contact" className="flex items-center gap-2 border-2 py-2 px-5 rounded-md border-blue-400">
+            <FaEnvelope className="text-blue-400" />
+            <p className="text-lg font-semibold">GET IN TOUCH</p>
+          </Link>
       </div>
 
-      <div>
-        
-      </div>
+        <hr className="w-full pb-5" />
+      <nav className="flex items-center justify-between text-lg">
+        <div className="flex gap-10">
+          <Link to="/">Home</Link>
+          <Link to="/blog">Blog</Link>
+          <Link to="/blog">Contact us</Link>
+        </div>
+        <div className="flex items-center gap-5">
+            <FaSearch className="text-2xl" />
+            <Link to="/login">Login</Link>
+        </div>
+      </nav>
     </div>
   );
 };
